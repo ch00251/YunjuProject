f653135520c36235a84f5ca97cf9858b872de13e
yunju <ch00251@naver.com>
2021-09-10 16:10:34 +0900

    *.do 적용안되서 처음부터 다시시작

--- project/src/main/java/com/yunju/project/users/dao/UsersDao.java
+++
@@ -1,9 +0,0 @@
-package com.yunju.project.users.dao;
-
-import com.yunju.project.users.dto.UsersDto;
-
-public interface UsersDao {
-	//회원 가입
-	public void insert(UsersDto dto);
-	
-}
--- project/src/main/java/com/yunju/project/users/service/UsersService.java
+++
@@ -1,8 +0,0 @@
-package com.yunju.project.users.service;
-
-import com.yunju.project.users.dto.UsersDto;
-
-public interface UsersService {
-	//회원가입
-	public void addUser(UsersDto dto);
-}
